751d2d035df239f11350df9f84fbf1bd9fe2ae35
Hemant Chaudhary <hemuchoudhary231@gmail.com>
2024-05-23 16:27:05 +0530

    User Profile Updated

--- frontend/src/components/userProfile.js
+++ frontend/src/components/userProfile.js
@@ -3,7 +3,7 @@ import React from 'react';
 const UserProfile = () => {
   return (
     <div className="container mx-auto mt-20 p-6 text-white">
-      <h1 className="text-3xl font-bold mb-6">Unfortunately there is no UserProfile Details 😢...But don't worry if I build for you that will work <span role="img" aria-label="smiley emoji">😊</span>.</h1>
+      <h1 className="text-3xl font-bold mb-6">Unfortunately there is no UserProfile Details 😢...But don't worry if I build for you that will work correctly <span role="img" aria-label="smiley emoji">😊</span>.</h1>
       <h1 className="text-3xl font-bold mb-6">I can use <span style={{ color: "rgb(246, 133, 59)" }}>Reactjs,Nextjs13,Tailwind CSS </span> for <span style={{ color: "rgb(246, 133, 59)" }}>frontend.</span></h1>
       <h1 className="text-3xl font-bold mb-6">For <span style={{ color: "rgb(246, 133, 59)" }}>Backend</span> <span style={{ color: "rgb(246, 133, 59)" }}>NextJs13 or Nodejs</span>   <span role="img" aria-label="rocket emoji">🚀</span> with  <span style={{ color: "rgb(246, 133, 59)" }}>Mysql or Mongodb </span> ,<span  style={{ color: "rgb(246, 133, 59)" }}>Nodemailer </span> as well.</h1>
       <h1 className="text-3xl font-bold mb-6">Give me a Try
