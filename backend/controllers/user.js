a4abf3fb18a45e2008dbef25fe695b7a4d7d4824
Amit_Bisht <singhbishtamit2@gmail.com>
2024-06-05 23:36:57 +0530

    redis

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,89 +4,79 @@ const orderData = require("../models/orderData");
 const User = require("../models/userRegistration");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const redis = require("redis");
+const Redis = require("ioredis");
+
+const redisClient = new Redis(process.env.REDIS_URL);
+redisClient.on("error", (err) => {
+  console.error("Redis Client Error", err);
+});
 async function getRes(req, res) {
-  const client = redis.createClient();
-  await client.connect();
   try {
     const { phone } = req.params;
-    const restaurent = await client.get(phone);
+    const restaurent = await redisClient.get(phone);
     if (restaurent) {
       res.json(JSON.parse(restaurent));
     } else {
-      // console.log("phone in backend", phone);
       const restorentInfo = await Model.find({ phone });
-      await client.setEx(phone, 1800, JSON.stringify(restorentInfo));
+      await redisClient.set(phone, JSON.stringify(restorentInfo), "EX", 1800);
       res.json(restorentInfo);
     }
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ message: "Internal server error" });
-  } finally {
-    await client.disconnect();
   }
 }
 
 async function getAllUser(req, res) {
   try {
     const Userinfo = await User.find({});
-    //console.log("User details", Userinfo);
     res.json(Userinfo);
   } catch (error) {
-    res.status(404).json({ message: "restaurant not found" });
+    res.status(404).json({ message: "User not found" });
   }
 }
+
 async function getUser(req, res) {
-  const client = redis.createClient();
-  await client.connect();
   try {
     const { phone } = req.params;
-    //console.log("phone of user", phone);
-    const user = await client.get(phone);
+    const user = await redisClient.get(phone);
     if (user) {
       res.json(JSON.parse(user));
     } else {
       const Userinfo = await User.find({ phone });
-      await client.setEx(phone, 1800, JSON.stringify(Userinfo));
-      //console.log("User details", Userinfo);
+      await redisClient.set(phone, JSON.stringify(Userinfo), "EX", 1800);
       res.json(Userinfo);
     }
   } catch (error) {
-    res.status(404).json({ message: "restaurant not found" });
-  } finally {
-    await client.disconnect();
+    res.status(404).json({ message: "User not found" });
   }
 }
+
 async function getAllRes(req, res) {
-  const client = redis.createClient();
-  await client.connect();
   try {
-    // const { input } = req.query;
     const key = "ami";
-    const data = await client.get(key);
+    const data = await redisClient.get(key);
     if (data) {
       res.json(JSON.parse(data));
     } else {
       const data = await Model.find({});
-      await client.setEx(key, 1800, JSON.stringify(data));
+      await redisClient.set(key, JSON.stringify(data), "EX", 1800);
       res.json(data);
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 }
+
 async function sendOrder(req, res) {
   try {
-    // Assuming req.body is an array of objects
     res.setHeader("Access-Control-Allow-Origin", "*");
     const verified = jwt.verify(req.body.token, process.env.TOKEN_KEY);
-    // console.log("phone sender", verified);
     for (const key in req.body) {
       if (key === "token") {
         continue;
       }
       const item = req.body[key];
-      console.log("data:", item);
       if (item.selected === true) {
         const data = new orderData({
           dishName: item.dishName,
@@ -99,34 +89,31 @@ async function sendOrder(req, res) {
         console.log("saved order", savedData);
       }
     }
-    res.status(200).json("order sent succesfully");
+    res.status(200).json("Order sent successfully");
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 }
+
 async function getAllDishes(req, res) {
-  const client = redis.createClient();
-  await client.connect();
   try {
     const { phone } = req.params;
-    // const { input } = req.query;
-    const data = await client.get(phone);
+    const data = await redisClient.get(phone);
     if (data) {
       res.json(JSON.parse(data));
     } else {
       const data = await Modeldish.find({ ph: phone });
-      await client.setEx(phone, 1800, JSON.stringify(data));
+      await redisClient.set(phone, JSON.stringify(data), "EX", 1800);
       res.json(data);
     }
   } catch (error) {
-    console.log("ERROR WHILE SERVING DISHES : ", error);
+    console.log("Error while serving dishes:", error);
     res.status(500).json({ message: error.message });
   }
 }
 
 async function getAllOrder(req, res) {
   try {
-    // const { input } = req.query;
     const data = await orderData.find({});
     res.json(data);
   } catch (error) {
@@ -137,9 +124,7 @@ async function getAllOrder(req, res) {
 async function getOrderdetails(req, res) {
   try {
     const phone = req.user;
-    console.log("USER", phone);
     const data = await orderData.find({ accept: true, phr: phone });
-    console.log("data", data);
     res.json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
